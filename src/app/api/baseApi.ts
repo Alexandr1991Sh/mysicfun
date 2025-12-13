b58c7c1c3b45b8a6904892a25d86a4aad45c5580
Alexandr1991Sh <an1991sh@yandex.ru>
2025-12-13 18:55:55 +0300

    Global error handling with baseQuery

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,4 +1,6 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {toast} from "react-toastify";
+import {isErrorWithProperty} from "@/common/utils";
 
 // `createApi` - функция из `RTK Query`, позволяющая создать объект `API`
 // для взаимодействия с внешними `API` и управления состоянием приложения
@@ -8,18 +10,49 @@ export const baseApi = createApi({
     reducerPath: 'baseApi',
     // `baseQuery` - конфигурация для `HTTP-клиента`, который будет использоваться для отправки запросов
     baseQuery: async (args, api, extraOptions) => {
-        await new Promise(resolve => setTimeout(resolve, 2000)) // delay
+        // await new Promise(resolve => setTimeout(resolve, 2000)) // delay
 
-        return fetchBaseQuery({
+        const result = await fetchBaseQuery({
             baseUrl: import.meta.env.VITE_BASE_URL,
             headers: {
-                'API-KEY': import.meta.env.VITE_API_KEY,
+                'API-KEY': import.meta.env.VITE_API_KEY ,
             },
             prepareHeaders: headers => {
                 headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
                 return headers
             },
         })(args, api, extraOptions)
+
+        if (result.error) {
+            switch (result.error.status) {
+                case 404:
+                    if (isErrorWithProperty(result.error.data, 'error')) {
+                        toast(result.error.data.error, { type: 'error', theme: 'colored' })
+                    } else {
+                        toast(JSON.stringify(result.error.data), { type: 'error', theme: 'colored' })
+                    }
+                    break
+
+                case 429:
+                    // ✅ 1. Type Assertions
+                    // toast((result.error.data as { message: string }).message, { type: 'error', theme: 'colored' })
+                    // ✅ 2. JSON.stringify
+                    // toast(JSON.stringify(result.error.data), { type: 'error', theme: 'colored' })
+                    // ✅ 3. Type Predicate
+                    if (isErrorWithProperty(result.error.data, 'message')) {
+                        toast(result.error.data.message, { type: 'error', theme: 'colored' })
+                    } else {
+                        toast(JSON.stringify(result.error.data), { type: 'error', theme: 'colored' })
+                    }
+                    break
+
+                default:
+                    toast('Some error occurred', { type: 'error', theme: 'colored' })
+            }
+        }
+
+        return result
+
     },
     tagTypes: ['Playlists'],
     endpoints: () => ({})
